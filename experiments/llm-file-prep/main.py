7bb56d25a8a453407427bc424496a8dce83eabb2
David Thompson <davidt@hey.com>
2025-10-09 21:17:12 +0100

    compare mammoth to markitdown

--- experiments/llm-file-prep/main.py
+++ experiments/llm-file-prep/main.py
@@ -1,9 +1,22 @@
 import mammoth
+from pathlib import Path
 
-with open("test.docx", "rb") as test_doc:
+HERE = Path(__file__).resolve().parent
+DOCX = HERE / "test.docx"
+with open(DOCX, "rb") as test_doc:
     result = mammoth.convert_to_html(test_doc)
     html = result.value
     messages = result.messages
 
+print('MAMMOTH')
 print(html)
 print(messages)
+
+from markitdown import MarkItDown
+
+md = MarkItDown(enable_plugins=False) # Set to True to enable plugins
+result = md.convert(DOCX)
+print('MARKITDOWN')
+print(result.text_content)
+print(result.markdown)
+print(result.title)
